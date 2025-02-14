dede4dab42e489216e16420d2cbda9788fe5adde
Andrés <168719669+andresllanga@users.noreply.github.com>
2025-02-14 20:24:16 +0000

    First Commit

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,18 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
 //Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap"
-
 // index.css'
 import '../styles/index.css'
-
 // components
 import Home from './components/Home';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Home/>
-  </React.StrictMode>,
-)
+import SecondsCounter from './components/SecondsCounter';
+var intervalID = setInterval(myCallback, 1000);
+let contador=0
+function myCallback() {
+  // Tu código debe ir aquí
+  // Los parámetros son totalmente opcionales
+  console.log("paso un segundo=60");
+  console.log("paso un segundo=1");
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <SecondsCounter seconds={contador}/>
+    </React.StrictMode>,
+  )
+  contador++
+  console.log(contador);
+  console.log(segundos);
+}
